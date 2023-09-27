3a05a51cc6aa913229ed8d5d5dd48a5425fc9619
ÄÄVedrana Avramovic <avramovicvedrana@gmail.com>
2023-09-27 20:53:50 +0200

    Add test: verify add employees

---
+++ src/main/java/d25_9_2023/Zadatak3.java
@@ -0,0 +1,41 @@
+package d25_9_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zadatak3 {
+    //Napisati program koji vrsi dodavanje 5 redova
+//Maksimizirati prozor
+//Ucitati stranicu https://www.tutorialrepublic.com/snippets/bootstrap/table-with-add-and-delete-row-feature.php
+//Dodati 5 redova sa istim podacima.Jedan red u jednoj iteraciji
+//Klik na dugme Add New
+//Unesite name,departmant i phone (uvek iste vrednost)
+//Trazenje po name atributu
+//Kliknite na zeleno Add dugme.
+//PAZNJA: Pogledajte strukturu stranice i videcete da se u svakom redu poslednje kolone javljaju dugmici edit, add, delete ali zbog prirode reda neki dugmici se vide a neki ne.
+//Morate da dohvatite uvek Add dugme iz poslednjeg reda tabele. Mozete koristeci index iz petlje, a mozete koristeci i last() fukncionalnost za xpath. Koristan link last mehnizma
+//Cekanje od 0.5s
+//Na kraju programa ugasite pretrazivac.
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.tutorialrepublic.com/snippets/bootstrap/table-with-add-and-delete-row-feature.php");
+
+        for (int i = 0; i < 5; i++) {
+            driver.findElement(By.cssSelector("div.col-sm-4 > button")).click();
+            driver.findElement(By.cssSelector("#name")).sendKeys("Jason Lee");
+            driver.findElement(By.cssSelector("#department")).sendKeys("Security");
+            driver.findElement(By.cssSelector("#phone")).sendKeys("+36540004568");
+
+            driver.findElement(By.xpath("//tr[last()]/td[4]/a[1]")).click();
+        }
+
+        Thread.sleep(500);
+
+        driver.quit();
+    }
+}
